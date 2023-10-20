85db4fa7c84fcd9e9602cacaabd10aab600d0267
Ovidiojcf <ojcfarias@gmail.com>
2023-10-20 08:57:53 -0300

    operações

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  visor : string = '0';
+  operacao! : number;
+  valor1! : number;
+  valor2! : number;
   constructor() {}
 
+  adicionarNumero(valor : string){
+    if((this.visor.length === 1) && (this.visor ==='0')){
+      this.visor = valor;
+    }else{
+      this.visor += valor;
+    }
+  }
+
+  adicionarOperacao(valor : number){
+    this.operacao = valor;
+    this.valor1 = +this.visor;
+    this.zerar();
+  }
+
+  calcular(){
+    this.valor2 = +this.visor;
+    switch(this.operacao){
+      case 0 : {
+        this.visor = "" + (this.valor1 + this.valor2);
+        break;
+      }
+      case 1 : {
+        this.visor = "" + (this.valor1 - this.valor2);
+        break;
+      }
+      case 2 : {
+        this.visor = "" + (this.valor1 * this.valor2);
+        break;
+      }
+      case 3 : {
+        this.visor = "" + (this.valor1 / this.valor2);
+        break;
+      }
+    }
+  }
+  zerar(){
+    this.visor = '0';
+  }
 }
